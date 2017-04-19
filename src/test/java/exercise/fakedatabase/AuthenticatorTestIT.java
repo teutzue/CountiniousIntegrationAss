3fd941bce34ad07ec16405dfe22b7cf3cf6a4a4a
teutzue_1995@yahoo.com <teutzue_1995@yahoo.com>
2017-04-19 13:20:29 +0200

    add db

--- src/test/java/exercise/fakedatabase/AuthenticatorTestIT.java
+++ src/test/java/exercise/fakedatabase/AuthenticatorTestIT.java
@@ -15,6 +15,10 @@ public class AuthenticatorTestIT extends AuthenticatorTest {
     @Override
     public Authenticator makeAuthenticator(){
         Mailer mailer = mock(Mailer.class);
+
+        if (System.getenv("TRAVIS") != null) {
+            return new Authenticator(new UserFacadeRealDB("pu_mySql_travis_Integration"), mailer);
+        }
         return new Authenticator(new UserFacadeRealDB("pu_localDB"), mailer);
     }
 
--- src/test/java/exercise/fakedatabase/UserFacadeTestIT.java
+++ src/test/java/exercise/fakedatabase/UserFacadeTestIT.java
@@ -9,12 +9,11 @@ import exercise.realdatabase.UserFacadeRealDB;
  * Created by CosticaTeodor on 19/04/2017.
  */
 public class UserFacadeTestIT extends UserFacadeTest {
-
     @Override
     public IUserFacade makeUserFacade() {
+        if (System.getenv("TRAVIS") != null) {
+            return new UserFacadeRealDB("pu_mySql_travis_Integration");
+        }
         return new UserFacadeRealDB("pu_localDB");
     }
-
-
-
 }
